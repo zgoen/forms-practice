eab572724764e6474e6894a457e452cbd14212db
zgoen <65671043+zgoen@users.noreply.github.com>
2020-06-04 13:58:41 -0500

    two way binding

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
+
 export class UserSettingsFormComponent implements OnInit {
 
+  userSettings : UserSettings ={
+    name: "Zeb",
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes and stuff'
+  }
   constructor() { }
 
   ngOnInit(): void {
