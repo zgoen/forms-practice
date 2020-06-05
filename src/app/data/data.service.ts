ec78127feee50b542837cd844f233c3169a701de
zgoen <65671043+zgoen@users.noreply.github.com>
2020-06-05 09:12:31 -0500

    form posting and data access

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]>{
+    return of(['Monthly', 'Annual', 'Lifetime']);
+  }
+
+  postUserSettingsForm(userSettings: UserSettings) : Observable<any>{
+  
+    return this.http.post('https://putsreq.com/hNecPM8IXWmhetcXKRr3', userSettings);
+
+  }
+
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -18,18 +20,38 @@ export class UserSettingsFormComponent implements OnInit {
   };
 
   userSettings : UserSettings = { ...this.originalUserSettings };
-  
-  constructor() { }
+  postError = false;
+  postErrorMessage = '';
+  subscriptionTypes: Observable<string[]>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.subscriptionTypes = this.dataService.getSubscriptionTypes(); 
   }
 
   onBlur(field : NgModel) {
     console.log('in onBlur: ', field.valid);
   }
 
+  onHttpError(errorResponse: any){
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+  }
+
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ', form.valid);
+    if(form.valid){
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('success: ' , result),
+        error => this.onHttpError(error)
+      );
+    }
+    else {
+      this.postError = true;
+      this.postErrorMessage = "Please fix the above errors playa"
+    }
   }
 
 }
